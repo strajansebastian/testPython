237d92ac26e425783dc9ae1b2a6f7decfee35ba6
Strajan Sebastian Ioan <strajan.sebastian@yahoo.com>
2013-04-17 11:54:05 +0300

    Tested new functions

--- python_regexp.py
+++ python_regexp.py
@@ -3,10 +3,20 @@ import pprint
 
 # print "Testing regexp\n"
 
-string_search = "This is a best test!!!"
-match = re.match('This', string_search)
+string_search = "This is the best test!!!this This THIS tHIS"
+match = re.search('This', string_search)
 pprint.pprint(match.group(0))
 print match.group(0)
+
+string_search = "This is the best test!!!this This THIS tHIS"
+match = re.match(r'(?P<THS>This) (is)', string_search)
+print match.group(0)
+print match.group(1, 2)
+print 'Name: ', match.group('THS')
+
+string_search = "This is the best test!!!this This THIS tHIS"
+match = re.findall('This', string_search, re.IGNORECASE)
+print match
 
 
 print '\nSubstitution\n'
